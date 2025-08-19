43df628e92dcd0e72682d80db9bde91cf2044b9f
mupsycho <amm@mupsy.net>
2025-08-19 09:54:04 +0800

    change page

--- src/view/introduction/index.vue
+++ src/view/introduction/index.vue
@@ -1,21 +1,23 @@
 <script setup lang="ts">
 import layout from "../../layout/default.vue";
-import P1 from "./p1.vue";
-import P2 from "./p2.vue";
-import P3 from "./p3.vue";
-import P4 from "./p4.vue";
-import P5 from "./p5.vue";
-import P6 from "./p6.vue";
+// import P1 from "./p1.vue";
+// import P2 from "./p2.vue";
+// import P3 from "./p3.vue";
+// import P4 from "./p4.vue";
+// import P5 from "./p5.vue";
+// import P6 from "./p6.vue";
+import P7 from "./p7.vue";
 </script>
 
 <template>
     <layout>
-        <P5></P5>
+        <P7></P7>
+        <!-- <P5></P5>
         <P1></P1>
         <P2></P2>
         <P3></P3>
         <P4></P4>
-        <P6></P6>
+        <P6></P6> -->
     </layout>
 </template>
 
